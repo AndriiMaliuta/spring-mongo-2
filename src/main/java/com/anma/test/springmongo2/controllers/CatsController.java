3548378e822608b1784044ad24569abe1ffd9e89
Andrii Maliuta <quadr988@gmail.com>
2020-07-18 18:36:39 +0300

    init

--- src/main/java/com/anma/test/springmongo2/controllers/CatsController.java
+++ src/main/java/com/anma/test/springmongo2/controllers/CatsController.java
@@ -1,13 +1,24 @@
 package com.anma.test.springmongo2.controllers;
 
+import com.anma.test.springmongo2.CatRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CatsController {
 
+    private final CatRepository catRepository;
+
+    public CatsController(CatRepository catRepository) {
+        this.catRepository = catRepository;
+    }
+
     @GetMapping("/cats")
-    public String getCatsPage() {
+    public String getCatsPage(Model model) {
+
+        model.addAttribute("cats", catRepository.findAll());
+
         return "cats";
     }
 
