23f40375720a32112681a2705735f918d480c8a2
Andrii Maliuta <quadr988@gmail.com>
2020-07-18 18:07:43 +0300

    init

--- src/main/java/com/anma/test/springmongo2/controllers/CatsController.java
+++ src/main/java/com/anma/test/springmongo2/controllers/CatsController.java
@@ -1,10 +1,12 @@
 package com.anma.test.springmongo2.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CatsController {
 
+    @GetMapping("/cats")
     public String getCatsPage() {
         return "cats";
     }
--- src/main/java/com/anma/test/springmongo2/controllers/HomeController.java
+++ src/main/java/com/anma/test/springmongo2/controllers/HomeController.java
@@ -1,10 +1,12 @@
 package com.anma.test.springmongo2.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
     public String getHomePage() {
         return "home";
     }
