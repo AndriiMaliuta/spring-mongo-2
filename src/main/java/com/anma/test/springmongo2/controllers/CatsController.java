5c18b9309a91b8bd335f16ca46830c3c0a7bd7fb
Andrii Maliuta <quadr988@gmail.com>
2020-07-19 14:49:40 +0300

    init

--- src/main/java/com/anma/test/springmongo2/controllers/CatsController.java
+++ src/main/java/com/anma/test/springmongo2/controllers/CatsController.java
@@ -1,9 +1,12 @@
 package com.anma.test.springmongo2.controllers;
 
+import com.anma.test.springmongo2.Cat;
 import com.anma.test.springmongo2.CatRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CatsController {
@@ -18,8 +21,23 @@ public class CatsController {
     public String getCatsPage(Model model) {
 
         model.addAttribute("cats", catRepository.findAll());
+        model.addAttribute("cat", new Cat());
 
         return "cats";
+    }
+
+
+    @PostMapping("/create-cat")
+    public String creatCat(@ModelAttribute Cat catModel, Model model) {
+
+        Cat newCat = new Cat();
+        newCat.setName(catModel.getName());
+        newCat.setColor(catModel.getColor());
+        newCat.setAge(catModel.getAge());
+
+        catRepository.save(newCat);
+
+        return "redirect:/cats";
     }
 
 }
